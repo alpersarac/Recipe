dd79e6376983965e83f6c6abfcdd27c9a5ae0fb2
alpersarac <alper.sarac42@gmail.com>
2023-06-01 23:59:37 +0300

    iteration through the list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  recipes:Recipe[] = [
+    new Recipe('Lahmacun','kıyma, domates','https://cdn.yemek.com/uploads/2020/06/tembel-lahamacunu-yemekcom.jpg')
+  ];
+  constructor() {
 
+  }
+  ngOnInit(){
+
+  }
 }
