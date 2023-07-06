94678ca8739ff689afc7f65159bd360cb9440c42
alpersarac <alper.sarac42@gmail.com>
2023-07-07 02:26:39 +0300

    Child route url id passing

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -8,11 +9,19 @@ import { RecipeService } from '../recipe.service';
   styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
-  @Input() recipe:Recipe;
+  recipe:Recipe;
+  id:number;
 
-  constructor(private recipeService:RecipeService) {  }
+  constructor(private recipeService:RecipeService, private route:ActivatedRoute) {  }
+
   ngOnInit(): void {
-
+    this.route.params
+      .subscribe(
+        (params:Params) => {
+          this.id = +params['id'];
+          this.recipe=this.recipeService.getRecipe(this.id);
+        }
+      );
   }
   onAddToShoppingList(){
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -31,6 +31,10 @@ export class RecipeService{
   getRecipes(){
     return this.recipes.slice();
   }
+
+  getRecipe(index:number){
+    return this.recipes[index];
+  }
   addIngredientsToShoppingList(ingredients:Ingredient[]){
     this.shoppingListService.addIngredients(ingredients);
   }
