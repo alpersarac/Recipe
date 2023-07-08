31c2d0f6aa354c13163d89ecefd4c16f0fe35e13
alpersarac <alper.sarac42@gmail.com>
2023-07-08 14:00:37 +0300

    Navigating new recipe and editting

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -12,7 +12,9 @@ export class RecipeDetailComponent implements OnInit {
   recipe:Recipe;
   id:number;
 
-  constructor(private recipeService:RecipeService, private route:ActivatedRoute) {  }
+  constructor(private recipeService:RecipeService,
+              private route:ActivatedRoute,
+              private router:Router) {  }
 
   ngOnInit(): void {
     this.route.params
@@ -25,5 +27,8 @@ export class RecipeDetailComponent implements OnInit {
   }
   onAddToShoppingList(){
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
+  onEditRecipe(){
+    this.router.navigate(['edit'], {relativeTo: this.route})
   }
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
 import { Component} from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-list',
@@ -10,10 +11,14 @@ import { RecipeService } from '../recipe.service';
 export class RecipeListComponent {
   recipes:Recipe[];
 
-  constructor(private recipeService:RecipeService) { }
+  constructor(private recipeService:RecipeService,
+              private router:Router,
+              private route:ActivatedRoute) { }
 
   ngOnInit(){
     this.recipes=this.recipeService.getRecipes();
   }
-
+  onNewRecipe(){
+    this.router.navigate(['new'], {relativeTo:this.route})
+  }
 }
