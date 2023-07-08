92fad97d266b1dedb42763ffb5b994314897ec9b
alpersarac <alper.sarac42@gmail.com>
2023-07-08 13:53:02 +0300

    understanding whether if we editting or adding

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-edit',
   templateUrl: './recipe-edit.component.html',
   styleUrls: ['./recipe-edit.component.css']
 })
-export class RecipeEditComponent {
+export class RecipeEditComponent implements OnInit {
+  id:number;
+  editMode=false;
 
+  constructor(private route:ActivatedRoute) {  }
+  ngOnInit(): void {
+    this.route.params
+      .subscribe(
+        (params: Params)=>{
+          this.id=+params['id'];
+          this.editMode= params['id']!=null;
+          console.log(this.editMode);
+        }
+      );
+  }
+  
 }
