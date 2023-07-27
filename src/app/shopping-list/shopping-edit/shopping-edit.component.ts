aae9fd46a2ea3a1e28bd4e516203e95cab9acb68
alpersarac <alper.sarac42@gmail.com>
2023-07-27 15:49:37 +0300

    ngmodel form

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,6 +1,7 @@
-import { Component, ElementRef,  ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -8,15 +9,14 @@ import { ShoppingListService } from '../shopping-list.service';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent {
-  @ViewChild('nameInput') nameInputRef:ElementRef;
-  @ViewChild('amountInput') amountInputRef:ElementRef;
+
 
   constructor(private shoppingListService:ShoppingListService) { }
 
-  onAddItem(){
-    const ingName=this.nameInputRef.nativeElement.value;
-    const ingAmount=this.amountInputRef.nativeElement.value;
-    const newIngredient = new Ingredient(ingName,ingAmount);
+  onAddItem(form:NgForm){
+    const value = form.value;
+
+    const newIngredient = new Ingredient(value.name,value.amount);
     this.shoppingListService.addShoppingList(newIngredient);
   }
 }
