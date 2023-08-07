a0fa943872d0a14bbf524b0b31c3af033f04fa5d
alpersarac <alper.sarac42@gmail.com>
2023-08-08 00:42:09 +0300

    Implementing http saving request

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,8 +1,17 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { RecipeService } from "../recipes/recipe.service";
 
 
  @Injectable({providedIn:'root'})
  export class DataStorageService{
-  constructor(private http: HttpClient){}
+  constructor(private http: HttpClient, private recipeService:RecipeService){}
+
+  storeRecipes(){
+    const recipes = this.recipeService.getRecipes();
+    this.http.put('https://recipe-8950d-default-rtdb.firebaseio.com/recipes.json',recipes).subscribe(response =>{
+      console.log(response);
+    });
+
+  }
  }
