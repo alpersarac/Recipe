9757dee00491a7229f5229377fcd72b37ef5c799
alpersarac <alper.sarac42@gmail.com>
2023-08-10 23:52:44 +0300

    empty recipe handling

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
+import { map } from "rxjs/operators";
 
 
  @Injectable({providedIn:'root'})
@@ -17,6 +18,11 @@ import { Recipe } from "../recipes/recipe.model";
   }
   fetchRecipes(){
     this.http.get<Recipe[]>('https://recipe-8950d-default-rtdb.firebaseio.com/recipes.json')
+    .pipe(map(recipes=>{
+      return recipes.map(recipe=>{
+        return {...recipe, ingredients: recipe.ingredients? recipe.ingredients:[]};
+      });
+    }))
     .subscribe(recipes => {
       console.log(recipes);
       this.recipeService.setRecipes(recipes)
