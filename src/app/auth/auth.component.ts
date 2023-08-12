ce42d6edcecc85c721a96abbc35a28840c91d9e8
alpersarac <alper.sarac42@gmail.com>
2023-08-12 20:05:49 +0300

    registration process and authservice update

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,18 +1,34 @@
 import { Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
+import { AuthService } from "./auth.service";
 
 @Component({
   selector:'app-auth',
   templateUrl:'./auth.component.html'
 })
 export class AuthComponent{
+  constructor(private authService:AuthService){}
   isLoginMode=true;
 
   onSwitchMode(){
     this.isLoginMode=!this.isLoginMode;
   }
   onSubmit(form: NgForm){
-    console.log(form);
+    if(!form.valid){
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+
+    if(!this.isLoginMode){
+      this.authService.signup(email,password)
+      .subscribe( response =>{
+      console.log(response);
+    }, error=>{
+      console.log(error);
+    });
+    }
+
     form.reset();
   }
 }
